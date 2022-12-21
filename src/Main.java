c401b4ad6ea05f2b9862efda9a7613d3602b3494
Gabriel Ferrari <gabrielferrari0001@gmail.com>
2022-12-21 19:30:45 -0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-
+import br.com.dio.desafio.oop.Curso;
 
 public class Main {
     public static void main(String[] args) {
-        
-    }
+        Curso curso1 = new Curso();
+        System.out.println(curso1);
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+    }   
     
 }
--- src/br/com/dio/desafio/oop/Curso.java
+++ src/br/com/dio/desafio/oop/Curso.java
@@ -1,5 +1,30 @@
 package br.com.dio.desafio.oop;
 public class Curso {
-    String titulo;
-    String descricao
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+    public String getTitulo(){
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria(){
+        return this.cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria){
+        this.cargaHoraria=cargaHoraria;
+    }
 }
